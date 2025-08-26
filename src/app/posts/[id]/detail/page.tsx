714cd8e0f2d3c1c0591cad30edb5269f6a6e07b6
Dev-FStacker <bennguyen.contact@gmail.com>
2025-08-26 23:25:52 +0700

    (fix) modify the height of post detail page.

--- src/app/posts/[id]/detail/page.tsx
+++ src/app/posts/[id]/detail/page.tsx
@@ -115,7 +115,7 @@ export default function PostDetailPage() {
   };
 
   return (
-    <div className="max-w-2xl mx-auto p-4">
+    <div className="max-w-2xl mx-auto p-4 min-h-screen relative">
       <Card
         variant="outlined"
         styles={{ body: { padding: 16 } }}
